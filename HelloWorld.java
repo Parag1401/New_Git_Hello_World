066b0a3d9122832fbe5705e8c1b816aeeaa75a8c
Parag <parag1401@gmail.com>
2020-07-31 17:39:02 +0000

    Added 3112344

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
 	public static void main(String pArgs[]) {
-		System.out.println("-----Hello World------------------");
+		System.out.println("----- Inserting Hello World------------------");
 	}
 }
